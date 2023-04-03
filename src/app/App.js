cac158558db3ae43b4c7e6facf5d3428b314ef0e
Никита Алферов <nikitaalferov@MacBook-Air-Nikita.local>
2023-04-03 21:36:09 +0300

    router-dom

--- src/app/App.js
+++ src/app/App.js
@@ -1,36 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import Users from './components/users'
-
-import api from './api'
+import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Users from './layouts/users'
+import Login from './layouts/login'
+import Main from './layouts/main'
+import NavBar from './components/navBar'
 
 function App() {
-	const [users, setUsers] = useState()
-	useEffect(() => {
-		api.users.fetchAll().then((data) => setUsers(data))
-	}, [])
-	const handleDelete = (userId) => {
-		setUsers(users.filter((user) => user._id !== userId))
-	}
-	const handleToggleBookMark = (id) => {
-		setUsers(
-			users.map((user) => {
-				if (user._id === id) {
-					return { ...user, bookmark: !user.bookmark }
-				}
-				return user
-			})
-		)
-		console.log(id)
-	}
 	return (
 		<div>
-			{users && (
-				<Users
-					onDelete={handleDelete}
-					onToggleBookMark={handleToggleBookMark}
-					users={users}
-				/>
-			)}
+			<NavBar />
+			<Switch>
+				<Route path="/users/:userId?" component={Users} />
+				<Route path="/login" component={Login} />
+				<Route path="/" exact component={Main} />
+				<Redirect to="/" />
+			</Switch>
 		</div>
 	)
 }
